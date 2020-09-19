d6b39e24e77e6e9afa08d89a5690ebd16982223d
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-09-19 13:35:03 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,23 @@ using Documenter, ClassicAlgorithmsCollections
 makedocs(
     modules = [ClassicAlgorithmsCollections],
     sitename = "ClassicAlgorithmsCollections.jl",
-    authors = ["Anselm Hahn <Anselm.Hahn@gmail.com>"],
-    doctest = true,
-    linkcheck = true,
-    format = Documenter.HTML(prettyurls = !("local" in ARGS)),
-    pages = [
-        "Home" => "index.md",
-        "Guide" => "man/guide.md",
-        "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md"],
-    ],
+    #authors = ["Anselm Hahn <Anselm.Hahn@gmail.com>"],
+    #doctest = true,
+    #linkcheck = true,
+    #format = Documenter.HTML(prettyurls = !("local" in ARGS)),
+    #pages = [
+    #    "Home" => "index.md",
+    #    "Guide" => "man/guide.md",
+    #    "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md"],
+    #],
 )
 
 
 deploydocs(
     repo = "https://github.com/Anselmoo/ClassicAlgorithmsCollections.git",
-    target = "build",
-    julia = "1.5",
-    osname = "linux",
-    deps = nothing,
-    make = nothing,
+#    target = "build",
+#    julia = "1.5",
+#    osname = "linux",
+#    deps = nothing,
+#    make = nothing,
 )
