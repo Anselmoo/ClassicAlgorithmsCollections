47baf7c19f60e7f602e6e89cd9245ea848f86296
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-09-19 14:02:06 +0200

    Extend make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,24 @@ using Documenter, ClassicAlgorithmsCollections
 makedocs(
     modules = [ClassicAlgorithmsCollections],
     sitename = "ClassicAlgorithmsCollections.jl",
-    #authors = ["Anselm Hahn <Anselm.Hahn@gmail.com>"],
-    #doctest = true,
-    #linkcheck = true,
-    #format = Documenter.HTML(prettyurls = !("local" in ARGS)),
-    #pages = [
-    #    "Home" => "index.md",
-    #    "Guide" => "man/guide.md",
-    #    "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md"],
-    #],
+    authors = "Anselm Hahn <Anselm.Hahn@gmail.com>",
+    doctest = true,
+    linkcheck = true,
+    format = Documenter.HTML(prettyurls = !("local" in ARGS)),
+    pages = [
+        "Home" => "index.md",
+        "Guide" => "man/guide.md",
+        "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md"],
+    ],
 )
 
 
 deploydocs(
     repo = "https://github.com/Anselmoo/ClassicAlgorithmsCollections.git",
-#    target = "build",
-#    julia = "1.5",
-#    osname = "linux",
+    target = "build",
+    julia = "1.5",
+    osname = "linux",
+    push_preview = true,
 #    deps = nothing,
 #    make = nothing,
 )
