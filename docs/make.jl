c2614a18393505cbc714c3ceec622fdf3a328d4e
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-09-19 13:05:31 +0200

    Re-Format and solve typos in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,36 +1,26 @@
 using Documenter, ClassicAlgorithmsCollections
 
+
 makedocs(
     modules = [ClassicAlgorithmsCollections],
     sitename = "ClassicAlgorithmsCollections.jl",
-    authors = ["Anselm Hahn <Anselm.Hahn@gmail.com>"]
+    authors = ["Anselm Hahn <Anselm.Hahn@gmail.com>"],
     doctest = true,
     linkcheck = true,
-    format = Documenter.HTML(prettyurls=!("local" in ARGS)),
+    format = Documenter.HTML(prettyurls = !("local" in ARGS)),
     pages = [
         "Home" => "index.md",
         "Guide" => "man/guide.md",
-        "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md",
-        #"man/examples.md",
-        #"man/syntax.md",
-        #"man/doctests.md",
-        #"man/latex.md",
-        #hide("man/hosting.md", [
-        #    "man/hosting/walkthrough.md"
-        #]),
-        #"man/other-formats.md",
-        ],
+        "Algorithms" => Any["Sorting and Searching"=>"man/SortingAndSearching.md"],
     ],
 )
 
-#deploydocs(repo = "github.com/JuliaLang/Example.jl.git")
-
 
 deploydocs(
-    repo    = "https://github.com/Anselmoo/ClassicAlgorithmsCollections.git",
-    target  = "build",
-    # julia   = "0.6",
-    # osname  = 'linux',
-    deps    = nothing,
-    make    = nothing,
+    repo = "https://github.com/Anselmoo/ClassicAlgorithmsCollections.git",
+    target = "build",
+    julia = "1.5",
+    osname = "linux",
+    deps = nothing,
+    make = nothing,
 )
